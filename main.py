19f0771d53f6a04d825282f3c5a75244f96db27f
Eleni <elenimariex@gmail.com>
2026-06-15 12:28:40 +0100

    Adds create_qr_code function

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import qrcode
 
 def get_qr_data():
     print("Enter QR code data:")
-    data = input()
-    print(data)
+    qr_data = input()
+    return qr_data
 
-# img = qrcode.make("Test")
-# type(img)
-# img.save("test_file.png")
+def create_qr_code(qr_data):
+    # img = qrcode.make("Test")
+    # type(img)
+    # img.save("test_file.png")
 
 if __name__ == "__main__":
-    get_qr_data()
+    qr_data = get_qr_data()
+    create_qr_code(qr_data=qr_data)
