ce025ac0e885cd88d2dfaa41cd8a152299c08c10
Eleni <elenimariex@gmail.com>
2026-06-15 15:14:48 +0100

    Adds file_name to QR creator func

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ def get_qr_info():
     qr_data = input()
     return {"file_name": file_name, "qr_data": qr_data}
 
-def create_qr_code(qr_data):
+def create_qr_code(file_name, qr_data):
     img = qrcode.make(qr_data)
     type(img)
-    img.save("test_file.png")
+    img.save(f"{file_name}.png")
 
 if __name__ == "__main__":
-    qr_data = get_qr_info()
-    create_qr_code(qr_data=qr_data)
+    file_name = get_qr_info()["file_name"]
+    qr_data = get_qr_info()["qr_data"]
+    create_qr_code(file_name=file_name, qr_data=qr_data)
