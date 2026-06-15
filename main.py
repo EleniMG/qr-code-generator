bcd26e7419bd106ac6e885b45f1b07928c57bda6
Eleni <elenimariex@gmail.com>
2026-06-15 15:12:43 +0100

    Adds file name input to qr_info function

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import qrcode
 
 def get_qr_info():
+    print("Enter QR code file name:")
+    file_name = input()
     print("Enter QR code data:")
     qr_data = input()
-    return qr_data
+    return {"file_name": file_name, "qr_data": qr_data}
 
 def create_qr_code(qr_data):
     img = qrcode.make(qr_data)
